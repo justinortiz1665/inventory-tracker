6b46436bea494457fb11213a3edb813107db7897
Justin Ortiz <justinortiz1665@gmail.com>
2025-04-10 18:56:03 +0000

    Restored to 'd93acda1332ba3583fab09a37c84f9c214ece792'
    Replit-Restored-To: d93acda1332ba3583fab09a37c84f9c214ece792

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/common/Sidebar";
+import Sidebar from "./components/Sidebar";
 import Dashboard from "./components/dashboard/Dashboard.js";
 import MainInventory from "./components/inventory/MainInventory";
 import "./App.css";
